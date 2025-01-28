00f82e3e6a9151bb2dffb96aefbfcc2af6d18b07
Tahmid11 <tahmid.uddin11@outlook.com>
2025-01-28 15:29:23 +0000

    Added comments

--- task_manager/src/App.jsx
+++ task_manager/src/App.jsx
@@ -14,8 +14,12 @@ function App() {
     setNewTasks([{id:uuidv4(), task: task},...previousTasks]);
   }
 
-  const deleteATask=(task)=>{
-    setNewTasks(previousTasks.filter((a)=>{return a.id!==task.id}));
+  const deleteATask=(taskToDelete)=>{
+    // Goes through the array and KEEPS every item whose ID does not match the id of the task I want to delete
+    // so if oneTask.id === taskToDelete,id then it's skipped over and left out the new array
+    // React re-renders using this new filtered array since theres a new Array which triggers React to update UI
+    
+    setNewTasks(previousTasks.filter((oneTask)=>{return oneTask.id!==taskToDelete.id}));
    
   }
 
